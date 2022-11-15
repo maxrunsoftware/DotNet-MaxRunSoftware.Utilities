b0f52cf8a31d07bd46f2a457c9b3abbb59cceb08
Steve <steven.d.foster@gmail.com>
2022-11-14 21:28:07 -0500

    Removed SQL Tests

--- MaxRunSoftware.Utilities.Tests/Utils/UtilReflectionTests.cs
+++ MaxRunSoftware.Utilities.Tests/Utils/UtilReflectionTests.cs
@@ -1,11 +1,11 @@
 // Copyright (c) 2022 Max Run Software (dev@maxrunsoftware.com)
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,13 +21,13 @@ public class UtilReflectionTests : TestBase
     [Fact]
     public void CreateInstance()
     {
-        var vRandom = Util.CreateInstance(typeof(Random));
+        var vRandom = Util.CreateInstanceFactory(typeof(Random))();
         Assert.NotNull(vRandom);
 
-        var vInt = Util.CreateInstance(typeof(int));
+        var vInt = Util.CreateInstanceFactory(typeof(int))();
         Assert.NotNull(vInt);
 
-        var vDateTime = Util.CreateInstance(typeof(DateTime));
+        var vDateTime = Util.CreateInstanceFactory(typeof(DateTime))();
         Assert.NotNull(vDateTime);
     }
 }
