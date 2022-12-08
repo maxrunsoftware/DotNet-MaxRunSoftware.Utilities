589fc427fc49039e84ca63b3ce29a80cf1532410
Steve <steven.d.foster@gmail.com>
2022-12-08 01:32:29 -0500

    Added Ftp.Tests

--- MaxRunSoftware.Utilities.Ftp.Tests/FtpTests.cs
+++ MaxRunSoftware.Utilities.Ftp.Tests/FtpTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2022 Max Run Software (dev@maxrunsoftware.com)
+// Copyright (c) 2022 Max Run Software (dev@maxrunsoftware.com)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,17 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// ReSharper disable RedundantUsingDirective.Global
-global using System;
-global using System.Data;
-global using System.Diagnostics;
-global using System.Collections;
-global using System.Collections.Generic;
-global using System.Collections.Immutable;
-global using System.IO;
-global using System.Linq;
-global using System.Reflection;
-global using System.Text;
-global using System.Threading;
-global using Xunit;
-global using Xunit.Abstractions;
+namespace MaxRunSoftware.Utilities.Ftp.Tests;
+
+public class FtpClientFtpTests : TestBase
+{
+    public FtpClientFtpTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { }
+
+    [Fact]
+    public void Can_Connect()
+    {
+
+    }
+
+}
