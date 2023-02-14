dce95980063ab0a430e1ef8e4121f93d7c00fdea
Steve <steven.d.foster@gmail.com>
2023-02-14 08:22:00 -0500

    Code Cleanup

--- MaxRunSoftware.Utilities.Common/Threading/CachedValue.cs
+++ MaxRunSoftware.Utilities.Common/Threading/CachedValue.cs
@@ -1,11 +1,11 @@
 // Copyright (c) 2023 Max Run Software (dev@maxrunsoftware.com)
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,9 +20,13 @@ public class CachedValue<T>
     private T? value;
     private bool hasValue;
     private readonly object locker = new();
-    public CachedValue(Func<T> func) => this.func = func;
+    public CachedValue(Func<T> func)
+    {
+        this.func = func;
+    }
 
     private bool isEnabled = true;
+
     public bool IsEnabled
     {
         get
@@ -61,7 +65,6 @@ public class CachedValue<T>
 
                 return value!;
             }
-
         }
     }
 }
--- MaxRunSoftware.Utilities.Common/Threading/ThreadBase.cs
+++ MaxRunSoftware.Utilities.Common/Threading/ThreadBase.cs
@@ -1,11 +1,11 @@
-﻿// Copyright (c) 2022 Max Run Software (dev@maxrunsoftware.com)
-//
+﻿// Copyright (c) 2023 Max Run Software (dev@maxrunsoftware.com)
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,9 +39,9 @@ public abstract class ThreadBase : IDisposable
     protected ThreadBase(ILoggerProvider loggerProvider)
     {
         log = loggerProvider.CreateLogger(GetType());
-        isStarted = new SingleUse(Locker);
-        isDisposed = new SingleUse(Locker);
-        thread = new Thread(WorkPrivate);
+        isStarted = new(Locker);
+        isDisposed = new(Locker);
+        thread = new(WorkPrivate);
         nameDefault = GetType().FullNameFormatted();
     }
 
